877bd3725da36b7da708b32dbb902151a76d89c6
sydney <sydneychase5841@gmail.com>
2021-04-18 16:38:45 -0400

    testing span

--- src/index.js
+++ src/index.js
@@ -18,12 +18,42 @@ function getpups() {
      
 }
 
-const pups = getpups();
-console.log(pups);
+// const pups = getpups();
+// console.log(pups);
 
+// 2. Create a component with the data
 
+/* <div id="dog-bar">
+<span id="dog-bar">Dog Name1</span>
+<span id="dog-bar">Dog Name2</span>
+<span id="dog-bar">Dog Name3</span>
 
-// const dogBarDiv = document.getElementById('dog-bar');
+</div>
+
+<div id="dog-summary-container">
+<h1>DOGGO:</h1>
+<div id="dog-info">
+  
+  <img src=dog_image_url>
+  <h2>Mr. Bonkers</h2>
+  <button>Good Dog!</button>
+*/
+function createPupDivBar(pupObj) {
+    const div = document.createElement('dog-bar');
+    span = document.createElement('span');
+
+    span.textContent = pupObj.name;
+    console.log(span);
+
+}
+const testpups = {
+    "id": 3,
+"name": "Skittles",
+"isGoodDog": true,
+"image": "https://curriculum-content.s3.amazonaws.com/js/woof-woof/dog_3.jpg"
+}
+  
+createPupDivBar(testpups);
 // const dogSpotDiv = document.getElementById('dog-info');
 // const filterBtn = document.getElementById('good-dog-filter');
 // const dbURL = 'http://localhost:3000/pups';
